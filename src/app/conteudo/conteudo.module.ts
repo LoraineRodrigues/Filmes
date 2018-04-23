e1cc4aff93cdeb2ba3a3ae55b2091123a6eb0785
LoraineRodrigues <loraine.rodrigues25@gmail.com>
2018-04-23 17:07:18 -0300

    Atualização

--- src/app/conteudo/conteudo.module.ts
+++ src/app/conteudo/conteudo.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ListaFilmesComponent } from './lista-filmes/lista-filmes.component';
 import { FilmeCardComponent } from './filme-card/filme-card.component';
 import { FilmeService } from './filme.service';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { FormPesquisaComponent } from './lista-filmes/form-pesquisa/form-pesquisa.component';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
+  
   ],
-  declarations: [ListaFilmesComponent, FilmeCardComponent],
+  declarations: [ListaFilmesComponent, FilmeCardComponent, FormPesquisaComponent],
   exports: [
     ListaFilmesComponent
   ],
